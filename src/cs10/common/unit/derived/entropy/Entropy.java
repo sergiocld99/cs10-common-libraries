65b3e63f09f8882e2ac1f64ff827569b91f225ce
sergiocarp10 <bs.sergiocalderon@gmail.com>
2021-01-07 22:41:17 -0300

    added Q/T entropy constructor

--- src/cs10/common/unit/derived/entropy/Entropy.java
+++ src/cs10/common/unit/derived/entropy/Entropy.java
@@ -1,8 +1,14 @@
 package cs10.common.unit.derived.entropy;
 
+import cs10.common.unit.base.temperature.Temperature;
 import cs10.common.unit.base.temperature.absolute.Kelvin;
 import cs10.common.unit.derived.SpecificUnit;
+import cs10.common.unit.derived.energy.Heat;
 
+/**
+ * @author Calderon Sergio Leandro
+ * @version 2
+ */
 public class Entropy extends SpecificUnit {
 
     public Entropy(boolean specific) {
@@ -12,6 +18,14 @@ public class Entropy extends SpecificUnit {
     public Entropy(double value, boolean specific){
         this(specific);
         setValue(value);
+    }
+
+    /**
+     * @param heat heat from hot source (negative) or to cold sink (positive)
+     * @param temperature temperature of hot source or cold sink
+     */
+    public Entropy(Heat heat, Temperature temperature){
+        this(heat.getValue() / temperature.getInCelsius().getValue(), true);
     }
 
     @Override
